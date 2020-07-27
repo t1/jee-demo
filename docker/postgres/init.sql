d36c411874d7a6d404bf613da2678ddc11d682c8
redDott <matthias.roth@ionos.com>
2020-07-27 15:52:43 +0200

    added postgres folder with init scripts

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -13,8 +13,37 @@ GRANT ALL PRIVILEGES ON DATABASE orders TO jee;
 \connect orders;
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA orders TO jee;
 
-
-
-
 \connect orders;
 SET search_path TO orders;
+
+CREATE TABLE orders.order
+(
+  id VARCHAR PRIMARY KEY,
+  orderDate TIMESTAMP NOT NULL,
+  FOREIGN KEY (persion_id) REFERENCES orders.person (id),
+  orderItemIds text[]
+);
+
+CREATE TABLE orders.orderItem
+(
+  id VARCHAR KEY,
+  ammount INT NOT NULL,
+  product VARCHAR,
+  pieceCostInCent INT
+);
+
+CREATE TABLE orders.person
+(
+  id VARCHAR PRIMARY KEY,
+  name  VARCHAR NOT NULL
+);
+
+CREATE INDEX orders_id
+	on orders.order (id);
+
+
+CREATE INDEX orderItems_id
+	on orders.orderItems (id);
+
+CREATE INDEX person_id
+	on orders.person (id);
