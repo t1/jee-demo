8c5eb8c4b442af4905ad65d734ecd04dd30c7bfc
redDott <matthias.roth@ionos.com>
2020-07-29 14:19:24 +0200

    refactor

--- app-backend/src/main/java/com/example/order/boundary/OrderBoundary.java
+++ app-backend/src/main/java/com/example/order/boundary/OrderBoundary.java
@@ -24,83 +24,77 @@ import com.example.order.domain.OrderRepository;
 @Timed
 @Path("/")
 public class OrderBoundary {
-    @Inject
-    OrderRepository repository;
+	@Inject
+	OrderRepository repository;
 
-    String cachedData;
-    private int calls;
-    private int breakCalls;
+	String cachedData;
+	private int calls;
+	private int breakCalls;
 
-    @PostConstruct
-    public void init() {
-        cachedData = UUID.randomUUID().toString();
-    }
+	@PostConstruct
+	public void init() {
+		cachedData = UUID.randomUUID().toString();
+	}
 
-    @GET
-    @Path("orders/{orderId}")
-    public Order getOrder(@PathParam("orderId") String orderId) throws InterruptedException {
-        return repository.getOrderById(orderId);
-    }
+	@GET
+	@Path("orders/{orderId}")
+	public Order getOrder(@PathParam("orderId") String orderId) throws InterruptedException {
+		return repository.getOrderById(orderId);
+	}
 
-    public String sorry() {
-        return "sorry this is a fallback";
-    }
+	@GET
+	@Path("timeout")
+	@Timeout(250)
+	public String getTimeout() {
+		randomSleep(400);
 
-    @GET
-    @Path("timeout")
-    @Timeout(250)
-    public String getTiemout() throws InterruptedException {
-        randomSleep(400);
+		return "Random string " + UUID.randomUUID().toString();
+	}
 
-        return "Random string " + UUID.randomUUID().toString();
-    }
+	@GET
+	@Path("timeoutRetry")
+	@Timeout(250)
+	@Retry(maxRetries = 2)
+	public String getTimeoutRetry() {
+		calls++;
+		randomSleep(600);
 
-    @GET
-    @Path("timeoutRetry")
-    @Timeout(250)
-    @Retry(maxRetries = 2)
-    public String getTiemoutRetry() throws InterruptedException {
-        calls++;
-        randomSleep(600);
+		return "Random string " + UUID.randomUUID().toString() + " method calls: " + calls;
+	}
 
-        return "Random string " + UUID.randomUUID().toString() + " method calls: " + calls;
-    }
+	@GET
+	@Path("timeoutFallback")
+	@Timeout(250)
+	@Fallback(fallbackMethod = "getCachedData")
+	public String getTimeoutFallback() {
+		randomSleep(400);
 
-    @GET
-    @Path("timeoutFallback")
-    @Timeout(250)
-    @Fallback(fallbackMethod = "getCachedData")
-    public String getTiemoutFallback() throws InterruptedException {
-        randomSleep(400);
+		return "Random string " + UUID.randomUUID().toString();
+	}
 
-        return "Random string " + UUID.randomUUID().toString();
-    }
+	@GET
+	@Path("circuitBreak")
+	@Timeout(250)
+	@Retry(maxRetries = 3)
+	@CircuitBreaker(
+			requestVolumeThreshold = 4,
+			failureRatio = 0.75,
+			delay = 30000,
+			successThreshold = 10)
+	public String breakCircuit() {
+		breakCalls++;
+		randomSleep(1000);
+		return "Random string " + UUID.randomUUID().toString() + " method calls: " + breakCalls;
+	}
 
-    @GET
-    @Path("circuitBreak")
-    @Timeout(250)
-    @Retry(maxRetries = 3)
-    @CircuitBreaker(successThreshold = 10,
-            requestVolumeThreshold = 4,
-            failureRatio = 0.75,
-            delay = 30000)
-    public String breakCircuit() {
-        breakCalls++;
-        randomSleep(1000);
-        return "Random string " + UUID.randomUUID().toString() + " method calls: " + breakCalls;
-    }
+	private void randomSleep(int sleep) {
+		try {
+			Thread.sleep(new Random().nextInt(sleep));
+		} catch (Exception e) {
+		}
+	}
 
-    private void randomSleep(int sleep) {
-        try {
-            Thread.sleep(new Random().nextInt(sleep));
-        } catch (Exception e) {
-            System.out.println("The application is taking too long...");
-
-        }
-    }
-
-    public String getCachedData() {
-
-        return "Random (cached) String is " + cachedData;
-    }
+	public String getCachedData() {
+		return "Random (cached) String is " + cachedData;
+	}
 }
